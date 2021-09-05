e3ee321596753bc82998c0e86f561a0f12af1734
Nadine <nadine.lisa.mathew@accenture.com>
2021-09-05 21:44:31 +0530

    local-storage-changes-for-theme

--- script.js
+++ script.js
@@ -5,37 +5,43 @@ const image1 = document.getElementById('image1');
 const image2 = document.getElementById('image2');
 const image3 = document.getElementById('image3');
 const textBox = document.getElementById('text-box');
-
-
-// Dark Mode Styles
-function darkMode(){
-    nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
-    textBox.style.backgroundColor = 'rgb(255 255 255/50%)';
-    toggleIcon.children[0].textContent = 'Dark Mode';
-    toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
-    image1.src = 'img/undraw_proud_coder_dark.svg';
-    image2.src = 'img/undraw_feeling_proud_dark.svg';
-    image3.src = 'img/undraw_conceptual_idea_dark.svg';
+const DARK_THEME = 'dark'
+const LIGHT_THEME = 'light'
+// Dark or Light Images
+function imageMode(color){
+    image1.src = `img/undraw_proud_coder_${color}.svg`;
+    image2.src = `img/undraw_feeling_proud_${color}.svg`;
+    image3.src = `img/undraw_conceptual_idea_${color}.svg`;
 }
-// Light Mode Styles
-function lightMode(){
-    nav.style.backgroundColor = 'rgb(255 255 255 / 50%)';
-    textBox.style.backgroundColor = 'rgb(0 0 0/50%)';
-    toggleIcon.children[0].textContent = 'Light Mode';
-    toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
-    image1.src = 'img/undraw_proud_coder_light.svg';
-    image2.src = 'img/undraw_feeling_proud_light.svg';
-    image3.src = 'img/undraw_conceptual_idea_light.svg';
+// Toggle Light & Dark Mode
+function toggleLightDarkModes(theme){
+    nav.style.backgroundColor = theme===LIGHT_THEME?'rgb(255 255 255 / 50%)': 'rgb(0 0 0 / 50%)';
+    textBox.style.backgroundColor = theme===LIGHT_THEME?'rgb(0 0 0/50%)':'rgb(255 255 255/50%)';
+    toggleIcon.children[0].textContent = theme===LIGHT_THEME?'Light Mode':'Dark Mode';
+    theme===LIGHT_THEME? toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun'):toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
+    theme===LIGHT_THEME? imageMode(LIGHT_THEME): imageMode(DARK_THEME);
 }
+
 // Switch Theme
 function switchTheme($event){
     if($event.target.checked){
-        document.documentElement.setAttribute('data-theme', 'dark');
-        darkMode();
+        document.documentElement.setAttribute('data-theme', DARK_THEME);
+        localStorage.setItem('theme', DARK_THEME);
+        toggleLightDarkModes(DARK_THEME);
     } else{
-        document.documentElement.setAttribute('data-theme', 'light');
-        lightMode();
+        document.documentElement.setAttribute('data-theme',LIGHT_THEME);
+        localStorage.setItem('theme', LIGHT_THEME);
+        toggleLightDarkModes(LIGHT_THEME);
     }
 }
 // Event Listener
 toggleSwitch.addEventListener('change',switchTheme);
+
+const currentTheme = localStorage.getItem('theme');
+if(currentTheme){
+    document.documentElement.setAttribute('data-theme', currentTheme);
+    if(currentTheme === DARK_THEME){
+        toggleSwitch.checked = true;
+        toggleLightDarkModes(DARK_THEME);
+    } 
+}
